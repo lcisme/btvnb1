f32cd906b9216e77bb804360398fe282b7222b6a
lcisme <92872523+lcisme@users.noreply.github.com>
2022-03-20 14:00:05 +0700

    Update bai3.java

--- company/bai3.java
+++ company/bai3.java
@@ -12,7 +12,7 @@ public class bai3 {
             return false;
         }
 
-        for(int i=2; i<=Math.sqrt(n); i++)
+        for(int i=2; i<=n/2; i++)
         {
             if(n%i==0){
                 return false;
